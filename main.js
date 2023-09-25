a92978cf845ef183aa7c0181359fe45d32d3ed60
AndreyAnklav <142445234+AndreyAnklav@users.noreply.github.com>
2023-09-25 20:38:33 +0300

    Заработал счетчик задач

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let tasks = document.querySelector(".tasks");
 let add = document.querySelector(".add");
 let toDoList = document.querySelector(".toDoList");
 
+
 add.onclick = function(){
   if(tasks.value.trim() === ''){
     alert('Введите значение')
@@ -18,23 +19,21 @@ add.onclick = function(){
     taskText.classList.toggle("textDecor")
   };
 
+  let green = document.querySelector(".green");
+  let x = Array.from(document.querySelectorAll(".task"));
+      green.value = x.length+1
   let deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Из листа";
   deleteButton.classList.add("delBut");
   deleteButton.onclick = () => {
     toDoList.removeChild(newTask);
+    green.value = x.length
   };
 
   newTask.appendChild(taskText);
   newTask.appendChild(deleteButton);
   toDoList.appendChild(newTask);
   
-
-let green = document.querySelector(".green")
-let x = document.querySelectorAll(".task")
-// for (let i = 0; i <= x.length; i++)
-green.value = x.length
-console.log(typeof x)
 }
     tasks.value = '';
   }
